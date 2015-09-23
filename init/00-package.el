61c82cd71cd32f6e929e94b62750e40fbebaca60
David Lukes <dafydd.lukes@gmail.com>
2015-09-23 15:59:55 +0200

    remove unused packages

--- init/00-package.el
+++ init/00-package.el
@@ -10,15 +10,15 @@
 (package-initialize)
 (package-refresh-contents)
 
-(defvar my-packages '(better-defaults paredit idle-highlight-mode
-  enh-ruby-mode flx-ido ido-ubiquitous find-file-in-project magit
-  projectile flx smex scpaste markdown-mode haml-mode org
-  company-jedi auctex cider undo-tree pandoc-mode clojure-mode
-  ess js2-mode php-mode slime web-mode editorconfig evil css-mode
-  neotree yaml-mode go-mode nyan-mode visual-regexp company
-  visual-regexp-steroids sunrise-commander sunrise-x-checkpoints
-  sunrise-x-tabs sunrise-x-loop sunrise-x-modeline yasnippet
-  zenburn-theme evil-commentary))
+(defvar my-packages '(better-defaults idle-highlight-mode
+  enh-ruby-mode flx-ido ido-ubiquitous magit projectile flx smex
+  scpaste markdown-mode haml-mode org company-jedi auctex cider
+  undo-tree pandoc-mode clojure-mode ess js2-mode php-mode slime
+  web-mode editorconfig evil css-mode neotree yaml-mode go-mode
+  nyan-mode visual-regexp company visual-regexp-steroids
+  sunrise-commander sunrise-x-checkpoints sunrise-x-tabs
+  sunrise-x-loop sunrise-x-modeline yasnippet zenburn-theme
+  evil-commentary))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
